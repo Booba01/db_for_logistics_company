1f66cfaa0f31e6b58999d30d154d6a1243ddd2a8
Booba01 <94732410+Booba01@users.noreply.github.com>
2023-10-19 23:41:42 +0400

    Delete warehouse/warehouse directory

--- warehouse/warehouse/src/main/java/org/udsu/warehouse/config/SwaggerConfig.java
+++
@@ -1,8 +0,0 @@
-package org.udsu.warehouse.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class SwaggerConfig {
-
-}
--- warehouse/warehouse/src/main/java/org/udsu/warehouse/repository/dto/StorageDTO.java
+++
@@ -1,9 +0,0 @@
-package org.udsu.warehouse.repository.dto;
-import lombok.Data;
-
-@Data
-public class StorageDTO {
-    private Long id;
-    private String name;
-    private String address;
-}
